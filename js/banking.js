4f68299f67177aa908d635964d01ff6bef866c9c
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-03-20 14:54:13 +0600

    balance

--- js/banking.js
+++ js/banking.js
@@ -1,3 +1,33 @@
 document.getElementById('deposit-button').addEventListener('click', function(){
-    console.log('clicked');
+
+    //deposit input field
+    const newDepositText = document.getElementById('deposit-input');
+    const newDepositValue = parseFloat(newDepositText.value);
+
+    //deposit output field
+    const previousDepositText = document.getElementById('deposit-output');
+    const previousDepositValue = parseFloat(previousDepositText.innerText);
+    
+    //adding the deposited amounts
+    const updatedDepositValue = newDepositValue + previousDepositValue;
+    
+    //show the update values
+    previousDepositText.innerText = updatedDepositValue;
+
+
+    //update account balance
+    const balanceText = document.getElementById('balance');
+    const balanceValue = parseFloat(balanceText.innerText);
+
+    const updatedBalance = balanceValue + newDepositValue;
+
+    balanceText.innerText = updatedBalance;
+
+
+
+
+    //clear the deposit input field
+    newDepositText.value = '';
+
+   
 });
