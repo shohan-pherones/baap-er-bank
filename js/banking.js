dc8211cd591d713af7f98fdc0ef03aacd439a227
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-03-20 15:14:38 +0600

    withdraw done

--- js/banking.js
+++ js/banking.js
@@ -31,3 +31,31 @@ document.getElementById('deposit-button').addEventListener('click', function(){
 
    
 });
+
+//handle withdraw
+document.getElementById('withdraw-button').addEventListener('click', function(){
+
+    //withdraw input section
+    const withdrawInputText = document.getElementById('withdraw-input');
+    const withdrawInputValue = parseFloat(withdrawInputText.value);
+    
+    //withdraw output section
+    const withdrawOutputText = document.getElementById('withdraw-amount');
+    const withdrawOutputValue = parseFloat(withdrawOutputText.innerText);
+    
+    //adding withdraw amounts
+    const updatedWithdraw = withdrawOutputValue + withdrawInputValue;
+
+    withdrawOutputText.innerText = updatedWithdraw;
+
+    //balance update after withdraw
+    const balanceText = document.getElementById('balance');
+    const balanceValue = parseFloat(balanceText.innerText);
+
+    const updatedBalance = balanceValue - withdrawInputValue;
+
+    balanceText.innerText = updatedBalance;
+
+    //clear the withdraw field
+    withdrawInputText.value = '';
+});
